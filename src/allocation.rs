bda588715f2c7738debd2927f77b30c7dd977a2b
Eugene Bokhan <eugenebokhan@icloud.com>
2026-01-28 16:54:05 +0000

    make allocation send and sync

--- src/allocation.rs
+++ src/allocation.rs
@@ -6,7 +6,11 @@ extern_protocol!(
     ///
     /// This protocol provides a common interface for adding Metal resources to residency sets.
     /// Call `MTLResidencySet::addAllocation` to add a Metal resource allocation to a residency set.
-    pub unsafe trait MTLAllocation: NSObjectProtocol {
+    ///
+    /// # Thread Safety
+    /// Metal resource objects (MTLBuffer, MTLTexture) are thread-safe and can be used from multiple threads.
+    /// See Apple's Metal Programming Guide for synchronization requirements when modifying contents.
+    pub unsafe trait MTLAllocation: NSObjectProtocol + Send + Sync {
         /// The size, in bytes, occupied by this allocation.
         #[unsafe(method(allocatedSize))]
         #[unsafe(method_family = none)]
